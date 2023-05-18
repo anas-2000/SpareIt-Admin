6996f1fc79179269eeec21a8ccc2e4d59fcd5590
Anas Abdullah <anas.abdullah815@gmail.com>
2023-05-18 15:32:27 +0500

    changed url to deployed url of backend

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 //const BASE_URL = "http://localhost:5000/api/";
-const BASE_URL = "http://localhost:3000/api/";
+const BASE_URL = "https://desert-sand-lemming-tam.cyclic.app/api/";
 // const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.accessToken;  // causing problem
 const user = JSON.parse(localStorage.getItem("persist:root"))?.user;
 const currentUser = user && JSON.parse(user).currentUser;
